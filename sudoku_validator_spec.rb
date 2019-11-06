cf8042f6f50b3f63c46170f0ab62282169e8d303
Robert J England II <robert.j.england.ii@gmail.com>
2019-11-06 00:29:41 -0500

    Added a whole slew of new tests for different sized grids.  Was contemplating adding a 36x36 but decided I didn't feel *that* bored.

--- sudoku_validator_spec.rb
+++ sudoku_validator_spec.rb
@@ -187,5 +187,265 @@ describe SudokuValidator do
       ].join("\n")
       @sudoku_validator.valid?(invalid_grid).must_equal false
     end
+
+    it "returns true when given a valid 8x8 grid" do
+      valid_grid = [
+        '1 2 3 4 | 5 6 7 8',
+        '5 6 7 8 | 1 2 3 4',
+        '--------+--------',
+        '2 3 4 1 | 6 7 8 5',
+        '6 7 8 5 | 2 3 4 1',
+        '--------+--------',
+        '3 4 1 2 | 7 8 5 6',
+        '7 8 5 6 | 3 4 1 2',
+        '--------+--------',
+        '4 1 2 3 | 8 5 6 7',
+        '8 5 6 7 | 4 1 2 3'
+      ].join("\n")
+      @sudoku_validator.valid?(valid_grid).must_equal true
+    end
+
+    it "returns false when given an invalid 8x8 grid (horizontal error)" do
+      invalid_grid = [
+        '1 2 3 4 | 5 6 7 8',
+        '5 6 7 8 | 1 2 3 4',
+        '--------+--------',
+        '2 3 4 1 | 6 7 8 5',
+        '6 7 8 5 | 2 3 4 1',
+        '--------+--------',
+        '3 4 1 2 | 7 8 5 2',
+        '7 8 5 6 | 3 4 1 6',
+        '--------+--------',
+        '4 1 2 3 | 8 5 6 7',
+        '8 5 6 7 | 4 1 2 3'
+      ].join("\n")
+      @sudoku_validator.valid?(invalid_grid).must_equal false
+    end
+
+    it "returns false when given an invalid 8x8 grid (vertical error)" do
+      invalid_grid = [
+        '1 2 3 4 | 5 6 7 8',
+        '5 6 7 8 | 1 2 3 4',
+        '--------+--------',
+        '2 3 4 1 | 6 7 8 5',
+        '6 7 8 5 | 2 3 4 1',
+        '--------+--------',
+        '3 4 1 2 | 7 8 5 6',
+        '7 8 5 6 | 3 4 1 2',
+        '--------+--------',
+        '4 1 2 3 | 8 5 6 7',
+        '8 5 6 7 | 4 2 1 3'
+      ].join("\n")
+      @sudoku_validator.valid?(invalid_grid).must_equal false
+    end
+
+    it "returns false when given an invalid 8x8 grid (box error)" do
+      invalid_grid = [
+        '1 2 3 4 | 5 6 7 8',
+        '5 6 7 8 | 1 2 3 4',
+        '--------+--------',
+        '2 3 4 1 | 6 7 8 5',
+        '6 7 8 5 | 2 3 4 1',
+        '--------+--------',
+        '3 4 1 2 | 7 8 5 6',
+        '4 1 2 3 | 8 5 6 7',
+        '--------+--------',
+        '7 8 5 6 | 3 4 1 2',
+        '8 5 6 7 | 4 1 2 3'
+      ].join("\n")
+      @sudoku_validator.valid?(invalid_grid).must_equal false
+    end
+
+    it "returns true when given a valid 16x16 grid" do
+      valid_grid = [
+        '0 1 2 3 | 4 5 6 7 | 8 9 a b | c d e f',
+        '4 5 6 7 | 8 9 a b | c d e f | 0 1 2 3',
+        '8 9 a b | c d e f | 0 1 2 3 | 4 5 6 7',
+        'c d e f | 0 1 2 3 | 4 5 6 7 | 8 9 a b',
+        '--------+---------+---------+--------',
+        '1 2 3 0 | 5 6 7 4 | 9 a b 8 | d e f c',
+        '5 6 7 4 | 9 a b 8 | d e f c | 1 2 3 0',
+        '9 a b 8 | d e f c | 1 2 3 0 | 5 6 7 4',
+        'd e f c | 1 2 3 0 | 5 6 7 4 | 9 a b 8',
+        '--------+---------+---------+--------',
+        '2 3 0 1 | 6 7 4 5 | a b 8 9 | e f c d',
+        '6 7 4 5 | a b 8 9 | e f c d | 2 3 0 1',
+        'a b 8 9 | e f c d | 2 3 0 1 | 6 7 4 5',
+        'e f c d | 2 3 0 1 | 6 7 4 5 | a b 8 9',
+        '--------+---------+---------+--------',
+        '3 0 1 2 | 7 4 5 6 | b 8 9 a | f c d e',
+        '7 4 5 6 | b 8 9 a | f c d e | 3 0 1 2',
+        'b 8 9 a | f c d e | 3 0 1 2 | 7 4 5 6',
+        'f c d e | 3 0 1 2 | 7 4 5 6 | b 8 9 a'
+      ].join("\n")
+      @sudoku_validator.valid?(valid_grid).must_equal true
+    end
+
+    it "returns false when given an invalid 16x16 grid (horizontal error)" do
+      invalid_grid = [
+        '0 1 2 3 | 4 5 6 7 | 8 9 a b | c d e f',
+        '4 5 6 7 | 8 9 a b | c d e f | 0 1 2 3',
+        '8 9 a b | c d e f | 0 1 2 3 | 4 5 6 7',
+        'c d e f | 0 1 2 3 | 4 5 6 7 | 8 9 a b',
+        '--------+---------+---------+--------',
+        '1 2 3 0 | 5 6 7 4 | 9 a b 8 | d e f c',
+        '5 6 7 4 | 9 a b 8 | d e f c | 1 2 3 0',
+        '9 a b 8 | d e f c | 1 2 3 0 | 5 6 7 4',
+        'd e f c | 1 2 3 0 | 5 6 7 4 | 9 a b 8',
+        '--------+---------+---------+--------',
+        '2 3 0 1 | 6 7 4 5 | a b 8 9 | e f c d',
+        '6 7 4 5 | a b 8 9 | e f c d | 2 3 0 1',
+        'a b 8 9 | e f c d | 2 3 0 1 | 6 b 4 5',
+        'e f c d | 2 3 0 1 | 6 7 4 5 | a 7 8 9',
+        '--------+---------+---------+--------',
+        '3 0 1 2 | 7 4 5 6 | b 8 9 a | f c d e',
+        '7 4 5 6 | b 8 9 a | f c d e | 3 0 1 2',
+        'b 8 9 a | f c d e | 3 0 1 2 | 7 4 5 6',
+        'f c d e | 3 0 1 2 | 7 4 5 6 | b 8 9 a'
+      ].join("\n")
+      @sudoku_validator.valid?(invalid_grid).must_equal false
+    end
+
+    it "returns false when given an invalid 16x16 grid (vertical error)" do
+      invalid_grid = [
+        '0 1 2 3 | 4 5 6 7 | 8 9 a b | c d e f',
+        '4 5 6 7 | 8 9 a b | c d e f | 0 1 2 3',
+        '8 9 a b | c d e f | 0 1 2 3 | 4 5 6 7',
+        'c d e f | 0 1 2 3 | 4 5 6 7 | 8 9 a b',
+        '--------+---------+---------+--------',
+        '1 2 3 0 | 5 6 7 4 | 9 a b 8 | d e f c',
+        '5 6 7 4 | 9 a b 8 | d e f c | 1 2 3 0',
+        '9 a b 8 | d e f c | 1 2 3 0 | 5 6 7 4',
+        'd e f c | 1 2 3 0 | 5 6 7 4 | 9 a b 8',
+        '--------+---------+---------+--------',
+        '2 3 0 1 | 6 7 4 5 | a b 8 9 | e f c d',
+        '6 7 4 5 | a b 8 9 | e f c d | 2 3 0 1',
+        'a b 8 9 | e f c d | 2 3 0 1 | 6 7 4 5',
+        'e f c d | 2 3 0 1 | 6 7 4 5 | a b 8 9',
+        '--------+---------+---------+--------',
+        '3 0 1 2 | 7 4 5 6 | b 8 9 a | f c d e',
+        '7 4 5 6 | b 8 9 a | f c d e | 3 0 1 2',
+        'b 8 9 a | f c d e | 3 0 1 2 | 7 5 4 6',
+        'f c d e | 3 0 1 2 | 7 4 5 6 | b 8 9 a'
+      ].join("\n")
+      @sudoku_validator.valid?(invalid_grid).must_equal false
+    end
+
+    it "returns false when given an invalid 16x16 grid (box error)" do
+      invalid_grid = [
+        '0 1 2 3 | 4 5 6 7 | 8 9 a b | c d e f',
+        '4 5 6 7 | 8 9 a b | c d e f | 0 1 2 3',
+        '8 9 a b | c d e f | 0 1 2 3 | 4 5 6 7',
+        'c d e f | 0 1 2 3 | 4 5 6 7 | 8 9 a b',
+        '--------+---------+---------+--------',
+        '1 2 3 0 | 5 6 7 4 | 9 a b 8 | d e f c',
+        '5 6 7 4 | 9 a b 8 | d e f c | 1 2 3 0',
+        '9 a b 8 | d e f c | 1 2 3 0 | 5 6 7 4',
+        'd e f c | 1 2 3 0 | 5 6 7 4 | 9 a b 8',
+        '--------+---------+---------+--------',
+        '2 3 0 1 | 6 7 4 5 | a b 8 9 | e f c d',
+        '6 7 4 5 | a b 8 9 | e f c d | 2 3 0 1',
+        'a b 8 9 | e f c d | 2 3 0 1 | 6 7 4 5',
+        'b 8 9 a | f c d e | 3 0 1 2 | 7 4 5 6',
+        '--------+---------+---------+--------',
+        '3 0 1 2 | 7 4 5 6 | b 8 9 a | f c d e',
+        '7 4 5 6 | b 8 9 a | f c d e | 3 0 1 2',
+        'e f c d | 2 3 0 1 | 6 7 4 5 | a b 8 9',
+        'f c d e | 3 0 1 2 | 7 4 5 6 | b 8 9 a'
+      ].join("\n")
+      @sudoku_validator.valid?(invalid_grid).must_equal false
+    end
+
+    it "returns true when given a valid 15x15 grid" do
+      valid_grid = [
+        '1 2 3 | 4 5 6 | 7 8 9 | a b c | d e f',
+        '4 5 6 | 7 8 9 | a b c | d e f | 1 2 3',
+        '7 8 9 | a b c | d e f | 1 2 3 | 4 5 6',
+        'a b c | d e f | 1 2 3 | 4 5 6 | 7 8 9',
+        'd e f | 1 2 3 | 4 5 6 | 7 8 9 | a b c',
+        '------+-------+-------+-------+------',
+        '2 3 1 | 5 6 4 | 8 9 7 | b c a | e f d',
+        '5 6 4 | 8 9 7 | b c a | e f d | 2 3 1',
+        '8 9 7 | b c a | e f d | 2 3 1 | 5 6 4',
+        'b c a | e f d | 2 3 1 | 5 6 4 | 8 9 7',
+        'e f d | 2 3 1 | 5 6 4 | 8 9 7 | b c a',
+        '------+-------+-------+-------+------',
+        '3 1 2 | 6 4 5 | 9 7 8 | c a b | f d e',
+        '6 4 5 | 9 7 8 | c a b | f d e | 3 1 2',
+        '9 7 8 | c a b | f d e | 3 1 2 | 6 4 5',
+        'c a b | f d e | 3 1 2 | 6 4 5 | 9 7 8',
+        'f d e | 3 1 2 | 6 4 5 | 9 7 8 | c a b'
+      ].join("\n")
+      @sudoku_validator.valid?(valid_grid).must_equal true
+    end
+
+    it "returns false when given an invalid 15x15 grid (horizontal error)" do
+      invalid_grid = [
+        '1 2 3 | 4 5 6 | 7 8 9 | a b c | d e f',
+        '4 5 6 | 7 8 9 | a b c | d e f | 1 2 3',
+        '7 8 9 | a b c | d e f | 1 2 3 | 4 5 6',
+        'a b c | d e f | 1 2 3 | 4 5 6 | 7 8 9',
+        'd e f | 1 2 3 | 4 5 6 | 7 8 9 | a b c',
+        '------+-------+-------+-------+------',
+        '2 3 1 | 5 6 4 | 8 9 7 | b c a | e f d',
+        '5 6 4 | 8 9 7 | b c a | e f d | 2 3 1',
+        '8 9 7 | b c a | e f d | 2 3 1 | 5 6 4',
+        'b c a | e f d | 2 3 1 | 5 6 4 | 8 9 7',
+        'e f d | 2 3 1 | 5 6 4 | 8 9 7 | b c a',
+        '------+-------+-------+-------+------',
+        '3 1 2 | 6 4 5 | 9 7 8 | c a b | f d e',
+        '6 4 5 | 9 7 8 | c a b | f d e | 3 1 2',
+        '9 7 8 | c a b | f d e | 3 1 2 | 9 4 5',
+        'c a b | f d e | 3 1 2 | 6 4 5 | 6 7 8',
+        'f d e | 3 1 2 | 6 4 5 | 9 7 8 | c a b'
+      ].join("\n")
+      @sudoku_validator.valid?(invalid_grid).must_equal false
+    end
+
+    it "returns false when given an invalid 15x15 grid (vertical error)" do
+      invalid_grid = [
+        '1 2 3 | 4 5 6 | 7 8 9 | a b c | d e f',
+        '4 5 6 | 7 8 9 | a b c | d e f | 1 2 3',
+        '7 8 9 | a b c | d e f | 1 2 3 | 4 5 6',
+        'a b c | d e f | 1 2 3 | 4 5 6 | 7 8 9',
+        'd e f | 1 2 3 | 4 5 6 | 7 8 9 | a b c',
+        '------+-------+-------+-------+------',
+        '2 3 1 | 5 6 4 | 8 9 7 | b c a | e f d',
+        '5 6 4 | 8 9 7 | b c a | e f d | 2 3 1',
+        '8 9 7 | b c a | e f d | 2 3 1 | 5 6 4',
+        'b c a | e f d | 2 3 1 | 5 6 4 | 8 9 7',
+        'e f d | 2 3 1 | 5 6 4 | 8 9 7 | b c a',
+        '------+-------+-------+-------+------',
+        '3 1 2 | 6 4 5 | 9 7 8 | c a b | f d e',
+        '6 4 5 | 9 7 8 | c a b | f d e | 3 1 2',
+        '9 7 8 | c a b | f d e | 3 1 2 | 6 4 5',
+        'c a b | f d e | 1 3 2 | 6 4 5 | 9 7 8',
+        'f d e | 3 1 2 | 6 4 5 | 9 7 8 | c a b'
+      ].join("\n")
+      @sudoku_validator.valid?(invalid_grid).must_equal false
+    end
+
+    it "returns false when given an invalid 15x15 grid (box error)" do
+      invalid_grid = [
+        '1 2 3 | 4 5 6 | 7 8 9 | a b c | d e f',
+        '4 5 6 | 7 8 9 | a b c | d e f | 1 2 3',
+        '7 8 9 | a b c | d e f | 1 2 3 | 4 5 6',
+        'a b c | d e f | 1 2 3 | 4 5 6 | 7 8 9',
+        'd e f | 1 2 3 | 4 5 6 | 7 8 9 | a b c',
+        '------+-------+-------+-------+------',
+        '2 3 1 | 5 6 4 | 8 9 7 | b c a | e f d',
+        '5 6 4 | 8 9 7 | b c a | e f d | 2 3 1',
+        'b c a | e f d | 2 3 1 | 5 6 4 | 8 9 7',
+        'e f d | 2 3 1 | 5 6 4 | 8 9 7 | b c a',
+        'c a b | f d e | 3 1 2 | 6 4 5 | 9 7 8',
+        '------+-------+-------+-------+------',
+        '3 1 2 | 6 4 5 | 9 7 8 | c a b | f d e',
+        '6 4 5 | 9 7 8 | c a b | f d e | 3 1 2',
+        '9 7 8 | c a b | f d e | 3 1 2 | 6 4 5',
+        '8 9 7 | b c a | e f d | 2 3 1 | 5 6 4',
+        'f d e | 3 1 2 | 6 4 5 | 9 7 8 | c a b'
+      ].join("\n")
+      @sudoku_validator.valid?(invalid_grid).must_equal false
+    end
   end
 end
