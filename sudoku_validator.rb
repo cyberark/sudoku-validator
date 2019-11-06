08dce1b72d11b01b0abf2f86e81e5fb85b05fac3
Robert J England II <robert.j.england.ii@gmail.com>
2019-11-06 01:27:52 -0500

    Added checks for properly formattedand constructed inputs, and a number of tests with errors to catch.

--- sudoku_validator.rb
+++ sudoku_validator.rb
@@ -12,6 +12,38 @@ class SudokuValidator
       end
     end
 
+    # check for proper input
+    if matrix.any? { |row| row.count != matrix.count }
+      puts "Malformed input - grid is not square"
+      return false
+    end
+    return false if matrix.flatten.uniq.count > matrix.count
+    if matrix.count % box_height != 0
+      puts "Malformed input - boxes are not uniform height"
+      return false
+    end
+    box_width = matrix.count / box_height
+    str.split("\n").each_with_index do |row, ix|
+      if ix % (box_height + 1) == box_height
+        if row =~ /[^-\+]/
+          puts "Malformed input - invalid character found in horizontal box line"
+          return false
+        end
+      else
+        row.split(" ").each_with_index do |col, jx|
+          if jx % (box_width + 1) == box_width
+            if col != "|"
+              puts "Malformed input - boxes are not uniform width"
+              return false
+            end
+          elsif col == "|"
+            puts "Malformed input - unexpected vertical box line"
+            return false
+          end
+        end
+      end
+    end
+
     # validate horizontal rows
     return false unless box_check(matrix, 1)
     # validate vertical rows
--- sudoku_validator_spec.rb
+++ sudoku_validator_spec.rb
@@ -447,5 +447,107 @@ describe SudokuValidator do
       ].join("\n")
       @sudoku_validator.valid?(invalid_grid).must_equal false
     end
+
+    it "returns false when given a nonsquare grid" do
+      valid_grid = [
+        '4 3 5 | 2 6 9 | 7 8 1',
+        '6 8 2 | 5 7 1 | 4 9 3',
+        '1 9 7 | 8 3 4 | 5 6 2',
+        '------+-------+------',
+        '8 2 6 | 1 9 5 | 3 4 7',
+        '3 7 4 | 6 8 2 | 9 1 5',
+        '9 5 1 | 7 4 3 | 6 2 8',
+        '------+-------+------',
+        '5 1 9 | 3 2 6 | 8 7 4',
+        '2 4 8 | 9 5 7 | 1 3 6',
+        '7 6 3 | 4 1 8 | 2 5 9 a'
+      ].join("\n")
+      @sudoku_validator.valid?(valid_grid).must_equal false
+    end
+
+    it "returns false when given a grid with too many values" do
+      valid_grid = [
+        '4 3 5 | 2 6 9 | 7 8 1',
+        '6 8 2 | 5 7 1 | 4 9 3',
+        '1 9 7 | 8 3 4 | 5 6 2',
+        '------+-------+------',
+        '8 2 6 | 1 9 5 | 3 4 7',
+        '3 7 4 | 6 8 2 | 9 1 5',
+        '9 5 1 | 7 4 3 | 6 2 8',
+        '------+-------+------',
+        '5 1 9 | 3 2 6 | 8 7 4',
+        '2 4 8 | 9 5 7 | 1 3 6',
+        '7 6 3 | 4 1 8 | 2 5 a'
+      ].join("\n")
+      @sudoku_validator.valid?(valid_grid).must_equal false
+    end
+
+    it "returns false when given a grid with nonuniform boxes (height)" do
+      valid_grid = [
+        '4 3 5 | 2 6 9 | 7 8 1',
+        '6 8 2 | 5 7 1 | 4 9 3',
+        '1 9 7 | 8 3 4 | 5 6 2',
+        '8 2 6 | 1 9 5 | 3 4 7',
+        '------+-------+------',
+        '3 7 4 | 6 8 2 | 9 1 5',
+        '9 5 1 | 7 4 3 | 6 2 8',
+        '------+-------+------',
+        '5 1 9 | 3 2 6 | 8 7 4',
+        '2 4 8 | 9 5 7 | 1 3 6',
+        '7 6 3 | 4 1 8 | 2 5 a'
+      ].join("\n")
+      @sudoku_validator.valid?(valid_grid).must_equal false
+    end
+
+    it "returns false when given a grid with an unexpected character in a box line" do
+      valid_grid = [
+        '4 3 5 | 2 6 9 | 7 8 1',
+        '6 8 2 | 5 7 1 | 4 9 3',
+        '1 9 7 | 8 3 4 | 5 6 2',
+        '------+-------+------',
+        '8 2 6 | 1 9 5 | 3 4 7',
+        '3 7 4 | 6 8 2 | 9 1 5',
+        '9 5 1 | 7 4 3 | 6 2 8',
+        '------+---x---+------',
+        '5 1 9 | 3 2 6 | 8 7 4',
+        '2 4 8 | 9 5 7 | 1 3 6',
+        '7 6 3 | 4 1 8 | 2 5 a'
+      ].join("\n")
+      @sudoku_validator.valid?(valid_grid).must_equal false
+    end
+
+    it "returns false when given a grid with nonuniform boxes (width)" do
+      valid_grid = [
+        '4 3 5 2 | 6 9 | 7 8 1',
+        '6 8 2 5 | 7 1 | 4 9 3',
+        '1 9 7 8 | 3 4 | 5 6 2',
+        '--------+-----+------',
+        '8 2 6 1 | 9 5 | 3 4 7',
+        '3 7 4 6 | 8 2 | 9 1 5',
+        '9 5 1 7 | 4 3 | 6 2 8',
+        '--------+-----+------',
+        '5 1 9 3 | 2 6 | 8 7 4',
+        '2 4 8 9 | 5 7 | 1 3 6',
+        '7 6 3 4 | 1 8 | 2 5 a'
+      ].join("\n")
+      @sudoku_validator.valid?(valid_grid).must_equal false
+    end
+
+    it "returns false when given a grid with an unexpected vertical line" do
+      valid_grid = [
+        '4 3 5 | 2 6 9 | 7 8 1',
+        '6 8 2 | 5 7 1 | 4 9 3',
+        '1 9 7 | 8 3 4 | 5 6 2',
+        '------+-------+------',
+        '8 2 6 | 1 9 5 | 3 4 7',
+        '3 7 4 | 6 8 2 | 9 1 5',
+        '9 5 1 | 7 4 3 | 6 2 8',
+        '------+-------+------',
+        '5 1 9 | 3 2 6 | 8 7 4',
+        '2 4 8 | 9 | 7 | 1 3 6',
+        '7 6 3 | 4 1 8 | 2 5 9'
+      ].join("\n")
+      @sudoku_validator.valid?(valid_grid).must_equal false
+    end
   end
 end
