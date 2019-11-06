50e276c6bcc06a5feb47dd4cb83bcf8ec4423d09
Robert J England II <robert.j.england.ii@gmail.com>
2019-11-05 22:20:37 -0500

    Replaced for loops with each-do loops.

--- sudoku_validator.rb
+++ sudoku_validator.rb
@@ -14,14 +14,14 @@ class SudokuValidator
     num_values = matrix.count
 
     # validate horizontal rows
-    for jx in 0..num_values-1
+    (0..num_values-1).each do |jx|
       if !box_check(matrix, jx, jx, 0, num_values-1)
         return false
       end
     end
 
     # validate vertical rows
-    for kx in 0..num_values-1
+    (0..num_values-1).each do |kx|
       if !box_check(matrix, 0, num_values-1, kx, kx)
         return false
       end
@@ -30,12 +30,12 @@ class SudokuValidator
     # validate squares
     if box_height != 0
       box_width = num_values / box_height
-      for jx in (0..num_values-box_height).step(box_height)
-       for kx in (0..num_values-box_width).step(box_width)
-         if !box_check(matrix, jx, jx + box_height - 1, kx, kx + box_width - 1)
-           return false
-         end
-       end
+      (0..num_values-box_height).step(box_height).each do |jx|
+        (0..num_values-box_width).step(box_width).each do |kx|
+          if !box_check(matrix, jx, jx + box_height - 1, kx, kx + box_width - 1)
+            return false
+          end
+        end
       end
     end
 
@@ -44,8 +44,8 @@ class SudokuValidator
 
   def box_check(matrix, row_begin, row_end, col_begin, col_end)
     row_items = []
-    for jx in row_begin..row_end
-      for kx in col_begin..col_end
+    (row_begin..row_end).each do |jx|
+      (col_begin..col_end).each do |kx|
         row_items.push(matrix[jx][kx])
       end
     end
