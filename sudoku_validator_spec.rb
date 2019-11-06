83e3bfdfc97aaa19504bb7d263972e38ee4efa72
Robert J England II <robert.j.england.ii@gmail.com>
2019-11-05 20:13:14 -0500

    Added more tests, to specifically test out each of the three kinds of checks.

--- sudoku_validator_spec.rb
+++ sudoku_validator_spec.rb
@@ -25,7 +25,7 @@ describe SudokuValidator do
     end
   end
 
-  it "returns false when given a valid grid" do
+  it "returns false when given an invalid grid (original test)" do
     valid_grid = [
       '3 3 5 | 2 6 9 | 7 8 1',
       '3 8 2 | 5 7 1 | 4 9 3',
@@ -36,6 +36,57 @@ describe SudokuValidator do
       '9 5 1 | 7 4 3 | 6 2 8',
       '------+-------+------',
       '5 1 9 | 3 2 6 | 8 7 4',
+      '2 4 8 | 9 5 7 | 1 3 6',
+      '7 6 3 | 4 1 8 | 2 5 9'
+    ].join("\n")
+    @sudoku_validator.valid?(valid_grid).must_equal false
+  end
+
+  it "returns false when given an invalid grid (horizontal error)" do
+    valid_grid = [
+      '4 3 5 | 2 6 9 | 7 8 1',
+      '6 8 2 | 5 7 1 | 4 9 3',
+      '1 9 7 | 8 3 4 | 5 6 2',
+      '------+-------+------',
+      '8 2 6 | 1 9 5 | 3 4 7',
+      '3 7 4 | 6 8 2 | 9 1 5',
+      '9 5 1 | 7 4 3 | 6 2 8',
+      '------+-------+------',
+      '5 1 9 | 3 2 6 | 8 7 4',
+      '2 4 8 | 9 5 7 | 1 3 9',
+      '7 6 3 | 4 1 8 | 2 5 6'
+    ].join("\n")
+    @sudoku_validator.valid?(valid_grid).must_equal false
+  end
+
+  it "returns false when given an invalid grid (vertical error)" do
+    valid_grid = [
+      '4 3 5 | 2 6 9 | 7 8 1',
+      '6 8 2 | 5 7 1 | 4 9 3',
+      '1 9 7 | 8 3 4 | 5 6 2',
+      '------+-------+------',
+      '8 2 6 | 1 9 5 | 3 4 7',
+      '3 7 4 | 6 8 2 | 9 1 5',
+      '9 5 1 | 7 4 6 | 3 2 8',
+      '------+-------+------',
+      '5 1 9 | 3 2 6 | 8 7 4',
+      '2 4 8 | 9 5 7 | 1 3 6',
+      '7 6 3 | 4 1 8 | 2 5 9'
+    ].join("\n")
+    @sudoku_validator.valid?(valid_grid).must_equal false
+  end
+
+  it "returns false when given an invalid grid (box error)" do
+    valid_grid = [
+      '4 3 5 | 2 6 9 | 7 8 1',
+      '6 8 2 | 5 7 1 | 4 9 3',
+      '1 9 7 | 8 3 4 | 5 6 2',
+      '------+-------+------',
+      '8 2 6 | 1 9 5 | 3 4 7',
+      '3 7 4 | 6 8 2 | 9 1 5',
+      '5 1 9 | 3 2 6 | 8 7 4',
+      '------+-------+------',
+      '9 5 1 | 7 4 3 | 6 2 8',
       '2 4 8 | 9 5 7 | 1 3 6',
       '7 6 3 | 4 1 8 | 2 5 9'
     ].join("\n")
