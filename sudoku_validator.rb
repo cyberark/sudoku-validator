23a564f38915f43bc2b2f5da1763854cef13f963
Robert J England II <robert.j.england.ii@gmail.com>
2019-11-05 22:49:45 -0500

    Refactored remaining range loops to times loops.

--- sudoku_validator.rb
+++ sudoku_validator.rb
@@ -14,21 +14,25 @@ class SudokuValidator
     num_values = matrix.count
 
     # validate horizontal rows
-    (0..num_values-1).each do |jx|
+    num_values.times do |jx|
       return false unless box_check(matrix, jx, 1, 0, num_values)
     end
 
     # validate vertical rows
-    (0..num_values-1).each do |kx|
+    num_values.times do |kx|
       return false unless box_check(matrix, 0, num_values, kx, 1)
     end
 
     # validate squares
     return true if box_height == 0
     box_width = num_values / box_height
-    (0..num_values-box_height).step(box_height).each do |jx|
-      (0..num_values-box_width).step(box_width).each do |kx|
-        return false unless box_check(matrix, jx, box_height, kx, box_width)
+    # For a sudoku grid with boxes X numbers tall and Y numbers wide, the grid
+    # will be XY*XY large and be Y boxes tall and X boxes wide.  Therefore if jx
+    # is the index of a row of boxes, I want to step jx by the box height X over
+    # box width Y cycles.
+    box_width.times do |jx|
+      box_height.times do |kx|
+        return false unless box_check(matrix, jx * box_height, box_height, kx * box_width, box_width)
       end
     end
 
