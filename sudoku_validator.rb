68a342b1abdebb70cfbe6c03643d6df30203e88d
Robert J England II <robert.j.england.ii@gmail.com>
2019-11-08 11:58:37 -0500

    Replaced double quotes with single quotes where necessary.  This and the following changes are things asked for by Rubocop, I didn't lint this prior to this commit because I thought it would be cheating.

--- sudoku_validator.rb
+++ sudoku_validator.rb
@@ -7,7 +7,7 @@ class SudokuValidator
     box_height = 0
     @matrix = []
     str.split("\n").each_with_index do |row, ix|
-      if !row.include? "+"
+      if !row.include? '+'
         @matrix.push(row.split(/ \| | /))
       elsif box_height == 0
         box_height = ix
@@ -49,30 +49,30 @@ class SudokuValidator
   def correctly_formatted_input?(str, box_height)
     # check for proper input
     if @matrix.any? { |row| row.count != @matrix.count }
-      puts "Malformed input - grid is not square"
+      puts 'Malformed input - grid is not square'
       return false
     end
     return false if @matrix.flatten.uniq.count > @matrix.count
     if @matrix.count % box_height != 0
-      puts "Malformed input - boxes are not uniform height"
+      puts 'Malformed input - boxes are not uniform height'
       return false
     end
     box_width = @matrix.count / box_height
     str.split("\n").each_with_index do |row, ix|
       if ix % (box_height + 1) == box_height
         if row =~ /[^-\+]/
-          puts "Malformed input - invalid character found in horizontal box line"
+          puts 'Malformed input - invalid character found in horizontal box line'
           return false
         end
       else
-        row.split(" ").each_with_index do |col, jx|
+        row.split(' ').each_with_index do |col, jx|
           if jx % (box_width + 1) == box_width
-            if col != "|"
-              puts "Malformed input - boxes are not uniform width"
+            if col != '|'
+              puts 'Malformed input - boxes are not uniform width'
               return false
             end
-          elsif col == "|"
-            puts "Malformed input - unexpected vertical box line"
+          elsif col == '|'
+            puts 'Malformed input - unexpected vertical box line'
             return false
           end
         end
