19c9ac56947f43780d420a408332512e0f23b1d1
Anbranin <keichelman@gmail.com>
2021-11-08 14:07:56 -0500

    start abstracting out to other methods

--- sudoku_validator.rb
+++ sudoku_validator.rb
@@ -1,28 +1,7 @@
 class SudokuValidator
   def valid?(str)
-    # validate horizontal rows
-    str.split("\n").each do |row|
-      next if row == '------+------+------'
 
-      row_items = []
-      row.split(' ').each do |col|
-        row_items.push(col) if col != '|'
-      end
-      return false if row_items.uniq.count < 9
-    end
-
-    # validate vertical rows
-    column_index = 0
-    9.times do
-      row_items = []
-      str.split("\n").each do |row|
-        row_items << row[column_index] if row != '------+------+------'
-      end
-      return false if row_items.uniq.count < 9
-
-      column_index += 2
-      column_index += 2 if str.split("\n").first[column_index] == '|'
-    end
+    return false unless valid_horizontal_rows?(str) && valid_vertical_rows?(str)
 
     # validate squares
     # squares blocks row 1
@@ -101,5 +80,35 @@ class SudokuValidator
     return false if row_items.uniq.count < 9
 
     true
+  end
+
+  private
+
+  def valid_horizontal_rows?(str)
+    # validate horizontal rows
+    str.split("\n").each do |row|
+      next if row == '------+------+------'
+
+      row_items = []
+      row.split(' ').each do |col|
+        row_items.push(col) if col != '|'
+      end
+      return false if row_items.uniq.count < 9
+    end
+  end
+
+  def valid_vertical_rows?(str)
+    # validate vertical rows
+    column_index = 0
+    9.times do
+      row_items = []
+      str.split("\n").each do |row|
+        row_items << row[column_index] if row != '------+------+------'
+      end
+      return false if row_items.uniq.count < 9
+
+      column_index += 2
+      column_index += 2 if str.split("\n").first[column_index] == '|'
+    end
   end
 end
