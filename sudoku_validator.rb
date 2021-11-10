c924991f285aa86b720279a1f04483c7f46b67d9
Anbranin <keichelman@gmail.com>
2021-11-10 10:27:25 -0500

    remove separator string so we don't have to constantly take it into account

--- sudoku_validator.rb
+++ sudoku_validator.rb
@@ -1,18 +1,18 @@
+require 'pry'
+
 class SudokuValidator
   def valid?(str)
-    return false unless valid_horizontal_rows?(str) && valid_vertical_rows?(str)
+    rows = str.split("\n").reject { |r| r == '------+------+------' }
 
-    rows = str.split("\n")
+    return false unless valid_horizontal_rows?(rows) && valid_vertical_rows?(str)
 
-    return valid_squares?(rows)
+    valid_squares?(rows)
   end
 
   private
 
-  def valid_horizontal_rows?(str)
-    str.split("\n").each do |row|
-      next if row == '------+------+------'
-
+  def valid_horizontal_rows?(rows)
+    rows.each do |row|
       row_items = []
       row.split(' ').each do |col|
         row_items.push(col) if col != '|'
@@ -26,7 +26,7 @@ class SudokuValidator
     9.times do
       row_items = []
       str.split("\n").each do |row|
-        row_items << row[column_index] if row != '------+------+------'
+        row_items << row[column_index]
       end
       return false if row_items.uniq.count < 9
 
@@ -36,7 +36,7 @@ class SudokuValidator
   end
 
   def valid_squares?(rows)
-    row_indices = [0..2, 4..6, 8..10]
+    row_indices = [0..2, 3..5, 6..8]
     column_indices = [0..5, 7..12, 15..20]
     valid_squares = row_indices.map do |row_index_range|
       column_indices.map do |column_index_range|
